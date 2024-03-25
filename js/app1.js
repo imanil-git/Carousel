6ae4eb4905ff11177fdcb343245f5cc54cbccdbf
Anil Rumdali Rai <anil.rai704757@gmail.com>
2024-03-25 22:58:30 +0545

    Carousel JavaScript Completed

--- js/app1.js
+++ js/app1.js
@@ -3,17 +3,46 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
 
-const images = document.querySelectorAll(".image")
-console.log('images');
+const images = document.querySelectorAll(".image");
+console.log("images");
 
 let slideNumber = 1;
+const length = images.length;
 
-right.addEventListener("click", ()=> {
-  if (slideNumber < images.length){
-    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
-    slideNumber++;
-  }else{
-    slider.style.transform = `translateX(0px)`;
-    slideNumber = 1;
-  };
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+  slideNumber++;
+};
+
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
+  slideNumber--;
+};
+
+const getFirstSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  slideNumber = 1;
+};
+
+const getLastSlide = () => {
+  slider.style.transform = `translateX(-${(length-1)*800}px)`;
+  slideNumber = length;
+};
+
+// right.addEventListener("click", () => {
+//   if (slideNumber < length) {
+//     nextSlide();
+//   } else {
+//     getFirstSlide();
+//   }
+// });
+
+
+// Ternary Operator
+right.addEventListener("click", ()=>{
+  slideNumber < length ? nextSlide() : getFirstSlide();
 });
+
+left.addEventListener("click", ()=> {
+  slideNumber > 1 ? prevSlide() : getLastSlide();
+})
